9f814ed8b2223ab2c4f22a192b5e5d1d8efc7504
Louis McCoy <louie@wellandlighthouse.com>
2018-08-21 15:45:51 -0500

    Basic Creds Check

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 /**
  * Takes a server image, infers the autoscalers and target groups associated and updates all the things for you
- * Usage: index.js <ip-address> | index.js -i <ip-address> | index.js -g <scaling-group-name>
+ * Usage: index.js [<ip-address>] [-r <region>] [-i <ip-address>] [-g <scaling-group-name>]
  */
 var AWS = require('aws-sdk');
-var ec2 = new AWS.EC2();
+var region = 'us-east-1';
 
-// Get our variables / flags in order
-// Make sure we have legits creds available to us
+// Get our variables / flags in order, ask if needed
+var ec2 = new AWS.EC2({region: region, apiVersion: '2016-11-15'});
+
+// Make sure we have legit creds available to us
+ec2.describeAvailabilityZones({}, function(err, data){
+	if (err) {
+		console.log('Please check your AWS Credentials are set according to https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/loading-node-credentials-shared.html');
+		process.exit(1);
+	} else {
+		console.log(data);
+	}
+})
 // Make sure our creds access all the things we need to access
 // Which AutoScaling Group we talkin' bout here?
 // Which Target Group we talkin' bout here?
 // Pick a server to use for the AMI
 // Pick a Launch Config to clone
 // Scale up, if needed
+// Maybe apt-get update && apt-get upgrade?
 // Make the AMI
 // Make the Launch Config
 // Swap out the Launch Config on the AS Group
